6613426eade2d7d399d43615ff2fc007ba7293d8
TheRealGuopher <jjguo@uw.edu>
2018-01-27 18:43:57 -0800

    Adding in count operation

--- SimpleCalcIOS/ViewController.swift
+++ SimpleCalcIOS/ViewController.swift
@@ -14,6 +14,9 @@ enum Operation:String {
     case Multiply = "*"
     case Divide = "/"
     case NULL = "Null"
+    case Avg
+    case Count
+    case Fact
 }
 
 class ViewController: UIViewController {
@@ -25,6 +28,8 @@ class ViewController: UIViewController {
     var rightValue = ""
     var result = ""
     var currentOperation:Operation = .NULL
+    var count = 0
+    var sum = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +41,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func numberPressed(_ sender: RoundButton) {
+    @IBAction func numberPressed(_ sender: RoundButton) { // everytime number is pressed, this adds to runningNumber
         if runningNumber.count <= 8 {
             runningNumber += "\(sender.tag)"
             outputLbl.text = runningNumber
@@ -51,12 +56,15 @@ class ViewController: UIViewController {
         outputLbl.text = "0"
     }
     @IBAction func dotPressed(_ sender: RoundButton) {
-        if runningNumber.count <= 7 {
+        if runningNumber.count <= 7 && !runningNumber.contains("."){
             runningNumber += "."
             outputLbl.text = runningNumber
         }
     }
     @IBAction func equalPressed(_ sender: RoundButton) {
+        if currentOperation == .Count {
+            specialOperation(operation: currentOperation)
+        }
         operation(operation: currentOperation)
     }
     @IBAction func addPressed(_ sender: RoundButton) {
@@ -71,12 +79,52 @@ class ViewController: UIViewController {
     @IBAction func dividePressed(_ sender: RoundButton) {
         operation(operation: .Divide)
     }
+    @IBAction func avgPressed(_ sender: RoundButton) {
+        
+    }
+    @IBAction func countPressed(_ sender: RoundButton) {
+        currentOperation = .Count
+        operation(operation: .Count)
+    }
+    @IBAction func factPressed(_ sender: RoundButton) {
+    }
+    
+    // make fact, avg, count separate func. Start at 0 or 1.
+    // make sure to check if leftValue/rightValue are empty or not empty, all the mutha fucking combos
+    // . + 3 doesn't break code
+    // Operations with just the dot, only treat it if there's numbers available
+    
+    func specialOperation(operation: Operation) {
+        if runningNumber == "" {
+            result = "\(count)"
+        } else {
+            result = "\(count + 1)"
+        }
+        runningNumber = ""
+        outputLbl.text = result
+        count = 0
+    }
     
     func operation(operation: Operation) {
-        if currentOperation != .NULL {
+        if currentOperation == .Avg || currentOperation == .Count || currentOperation == .Fact {
             if runningNumber != "" {
+                leftValue = runningNumber
+                runningNumber = ""
+                count += 1
+            }
+            
+//            if leftValue != runningNumber {
+//                leftValue = runningNumber
+//                runningNumber = ""
+//                result = "\(count + 1)"
+//                outputLbl.text = result
+//                count = 0
+//            }
+        } else if currentOperation != .NULL {
+            if runningNumber != "" && leftValue != "" {
                 rightValue = runningNumber
                 runningNumber = ""
+                
                 if currentOperation == .Add {
                     result = "\(Double(leftValue)! + Double(rightValue)!)"
                 } else if currentOperation == .Subtract {
@@ -86,14 +134,19 @@ class ViewController: UIViewController {
                 } else if currentOperation == .Divide {
                     result = "\(Double(leftValue)! / Double(rightValue)!)"
                 }
+                
                 leftValue = result
                 if (Double(result)!.truncatingRemainder(dividingBy: 1) == 0) {
                     result = "\(Int(Double(result)!))"
                 }
                 outputLbl.text = result
             }
+            if runningNumber != "" && leftValue == "" {
+                leftValue = runningNumber
+                runningNumber = ""
+            }
             currentOperation = operation
-        } else {
+        } else { // "8 +" or "+"
             leftValue = runningNumber
             runningNumber = ""
             currentOperation = operation
